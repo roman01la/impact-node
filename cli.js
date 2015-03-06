430ffc962d2b9628dfe1a00ad9168236b16a55c9
Roman Liutikov <roman01la@romanliutikov.com>
2015-03-06 18:28:19 +0200

    Add -v, -h and default output as -h

--- cli.js
+++ cli.js
@@ -3,6 +3,9 @@ var helpers = require('./lib/helpers');
 var args = process.argv,
     task = args[2];
 
+var v = '1.2.2',
+    help = '\nUsage:\n\nimpact-node create:project {path to impact dir}\nimpact-node create:entity {name}\nimpact-node serve\nimpact-node serve -p 9000\nimpact-node serve -watch\nimpact-node build';
+
 (/create:/).test(task) && require('./lib/create')(args[2].split(':')[1], args[3]);
 
 switch (task) {
@@ -18,6 +21,24 @@ switch (task) {
     case 'build':
 
         require('./lib/build')();
+
+        break;
+
+    case '-v':
+
+        console.log(v);
+
+        break;
+
+    case '-h':
+
+        console.log(help);
+
+        break;
+
+    default:
+
+        console.log(help);
 
         break;
 }
