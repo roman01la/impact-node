92e601a4512c64c83c10b6ae99cb9cd1563debdd
Roman Liutikov <roman01la@romanliutikov.com>
2015-03-24 01:46:55 +0200

    eslintify

--- cli.js
+++ cli.js
@@ -6,7 +6,10 @@ var args = process.argv,
 var v = '1.2.2',
     help = '\nUsage:\n\nimpact-node create:project {path to impact dir}\nimpact-node create:entity {name}\nimpact-node serve\nimpact-node serve -p 9000\nimpact-node serve -watch\nimpact-node build';
 
-(/create:/).test(task) && require('./lib/create')(args[2].split(':')[1], args[3]);
+if ((/create:/).test(task)) {
+
+    return require('./lib/create')(args[2].split(':')[1], args[3]);
+}
 
 switch (task) {
 
